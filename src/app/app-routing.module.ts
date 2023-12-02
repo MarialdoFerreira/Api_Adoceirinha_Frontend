146f8b55018bf840a7d26359459bf9156d39ea32
marcos almeida <marcos.santos@agrosatelite.com.br>
2023-12-02 03:00:09 -0300

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NewItemFormComponent } from './components/new-item-form/new-item-form.component';
+import { ListItemsComponent } from './components/list-items/list-items.component';
+import { ItemViewComponent } from './components/item-view/item-view.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'novo', component: NewItemFormComponent },
+  { path: '', component: ListItemsComponent},
+  { path: 'doce/:id', component: ItemViewComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NewItemFormComponent } from './components/new-item-form/new-item-form.component';
+import { ListItemsComponent } from './components/list-items/list-items.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { ItemViewComponent } from './components/item-view/item-view.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NewItemFormComponent,
+    ListItemsComponent,
+    ItemViewComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
